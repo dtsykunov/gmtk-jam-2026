bdf4c2af2548a1202169b22b33f32e281dce80c7
dtsykunov <7431448-dtsykunov@users.noreply.gitlab.com>
2026-07-23 17:09:35 +0000

    control character

--- scenes/character/character.gd
+++ scenes/character/character.gd
@@ -1,6 +1,22 @@
 extends Node2D
 
+enum Stance {
+	SITTING,
+	STANDING,
+}
+
+enum ArmPose {
+	RAISED,
+	LOWERED,
+}
+
+var arm_l := ArmPose.LOWERED
+var arm_r := ArmPose.LOWERED
+
+var stance := Stance.STANDING
+
 @onready var anim_player: AnimationPlayer = %AnimationPlayer
+@onready var anim_tree: AnimationTree = %AnimationTree
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,15 +25,23 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("move_left"):
-		anim_player.play("arm_l_up")
-	elif Input.is_action_just_pressed("move_right"):
-		anim_player.play("arm_r_up")
-	elif Input.is_action_just_released("move_left"):
-		anim_player.play_backwards("arm_l_up")
-	elif Input.is_action_just_released("move_right"):
-		anim_player.play_backwards("arm_r_up")
-	elif Input.is_action_just_pressed("move_down"):
-		anim_player.play("sit")
+	if Input.is_action_just_pressed("move_down"):
+		stance = Stance.SITTING
+		anim_tree["parameters/sit_blend/blend_amount"] = 1.0
 	elif Input.is_action_just_released("move_down"):
-		anim_player.play_backwards("sit")
+		stance = Stance.STANDING
+		anim_tree["parameters/sit_blend/blend_amount"] = 0.0
+
+	if Input.is_action_just_pressed("move_right"):
+		arm_l = ArmPose.RAISED
+		anim_tree["parameters/arm_l_blend/blend_amount"] = 1.0
+	elif Input.is_action_just_released("move_right"):
+		arm_l = ArmPose.LOWERED
+		anim_tree["parameters/arm_l_blend/blend_amount"] = 0.0
+
+	if Input.is_action_just_pressed("move_left"):
+		arm_r = ArmPose.RAISED
+		anim_tree["parameters/arm_r_blend/blend_amount"] = 1.0
+	elif Input.is_action_just_released("move_left"):
+		arm_r = ArmPose.LOWERED
+		anim_tree["parameters/arm_r_blend/blend_amount"] = 0.0
