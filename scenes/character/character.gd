7991e6bc0955b5a4c077a1c58f5e3f0d82230860
dtsykunov <7431448-dtsykunov@users.noreply.gitlab.com>
2026-07-23 17:56:36 +0000

    almost mvp

--- scenes/character/character.gd
+++ scenes/character/character.gd
@@ -16,24 +16,31 @@ enum ArmPose {
 var arm_l := ArmPose.LOWERED:
 	set(value):
 		arm_l = value
-		if value == ArmPose.LOWERED:
-			anim_tree["parameters/arm_l_blend/blend_amount"] = 0.0
-		elif value == ArmPose.RAISED:
-			anim_tree["parameters/arm_l_blend/blend_amount"] = 1.0
+		match (value):
+			ArmPose.LOWERED:
+				anim_tree["parameters/arm_l_blend/blend_amount"] = 0.0
+			ArmPose.RAISED:
+				anim_tree["parameters/arm_l_blend/blend_amount"] = 1.0
 
 
 var arm_r := ArmPose.LOWERED:
 	set(value):
 		arm_r = value
-		if value == ArmPose.LOWERED:
-			anim_tree["parameters/arm_r_blend/blend_amount"] = 0.0
-		elif value == ArmPose.RAISED:
-			anim_tree["parameters/arm_r_blend/blend_amount"] = 1.0
+		match value:
+			ArmPose.LOWERED:
+				anim_tree["parameters/arm_r_blend/blend_amount"] = 0.0
+			ArmPose.RAISED:
+				anim_tree["parameters/arm_r_blend/blend_amount"] = 1.0
 
 var stance := Stance.STANDING:
 	set(value):
 		stance = value
-		if value == Stance.STANDING:
-			anim_tree["parameters/sit_blend/blend_amount"] = 0.0
-		elif value == Stance.SITTING:
-			anim_tree["parameters/sit_blend/blend_amount"] = 1.0
+		match value:
+			Stance.STANDING:
+				anim_tree["parameters/sit_blend/blend_amount"] = 0.0
+			Stance.SITTING:
+				anim_tree["parameters/sit_blend/blend_amount"] = 1.0
+
+
+func has_same_stance(other: Character) -> bool:
+	return [arm_l, arm_r, stance] == [other.arm_l, other.arm_r, other.stance]
---
+++ scenes/game_scene/game_controller.gd
@@ -0,0 +1,35 @@
+extends Node
+
+@export var player: Character
+@export var enemy: Character
+
+@onready var countdown: Timer = $Countdown
+
+@onready var countdown_label: Label = %CountdownLabel
+@onready var score_label: Label = %ScoreLabel
+
+var score := 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	give_enemy_random_stance()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	countdown_label.text = str(ceili(countdown.time_left))
+
+
+func give_enemy_random_stance() -> void:
+	enemy.arm_l = Character.ArmPose.values().pick_random()
+	enemy.arm_r = Character.ArmPose.values().pick_random()
+	enemy.stance = Character.Stance.values().pick_random()
+
+
+func _on_countdown_timeout() -> void:
+	if enemy.has_same_stance(player):
+		score += 1
+		score_label.text = str(score)
+
+	countdown.start()
+	give_enemy_random_stance()
