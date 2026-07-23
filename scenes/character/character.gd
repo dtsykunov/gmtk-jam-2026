c60ebcb94196ac3c6f3618d17f6d80cf46deb8eb
dtsykunov <7431448-dtsykunov@users.noreply.gitlab.com>
2026-07-23 13:40:12 +0000

    basic animation

--- scenes/character/character.gd
+++ scenes/character/character.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var anim_player: AnimationPlayer = %AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,5 +8,16 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("move_left"):
+		anim_player.play("arm_l_up")
+	elif Input.is_action_just_pressed("move_right"):
+		anim_player.play("arm_r_up")
+	elif Input.is_action_just_released("move_left"):
+		anim_player.play_backwards("arm_l_up")
+	elif Input.is_action_just_released("move_right"):
+		anim_player.play_backwards("arm_r_up")
+	elif Input.is_action_just_pressed("move_down"):
+		anim_player.play("sit")
+	elif Input.is_action_just_released("move_down"):
+		anim_player.play_backwards("sit")
